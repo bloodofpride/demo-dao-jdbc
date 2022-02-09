cc4f685261edb36d0585ce8f8bce841a3cf1859a
bloodofpride <antonio_maxwell@hotmail.com>
2022-02-09 12:09:14 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
-        System.out.println(dp);
+        Seller s = new Seller(21, "Bob", "bob@gamil.com", new Date(), 3000.0, dp);
+        System.out.println(s);
     }
 }
