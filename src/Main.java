2738f0bcfdc45b50fb7d21758a878c7fb9fd9708
bloodofpride <antonio_maxwell@hotmail.com>
2022-02-09 13:45:47 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
         Seller s = new Seller(21, "Bob", "bob@gamil.com", new Date(), 3000.0, dp);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(s);
     }
 }
