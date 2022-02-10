8ad3d0ce4777cf0dc2af4f33a54a484fb00e3504
bloodofpride <antonio_maxwell@hotmail.com>
2022-02-10 11:03:19 -0300

    findById implementation department

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(10);
         System.out.println(seller);
+
+        System.out.println("=== TEST 2: department findById ====");
+        Department dep = departmentDao.findById(4);
+        System.out.println(dep);
     }
 }
